61f4c15aca0abe2954ffc9e92285e4d90f4ebcd5
Tyler-Ahmed <145777684+Tyler-Ahmed@users.noreply.github.com>
2024-04-08 20:07:59 +0000

    Pending changes exported from your codespace

---
+++ Final_Project.py
@@ -0,0 +1,33 @@
+
+
+"""Create quiz class here and define the methods"""
+class Quiz:
+    def __init__(self, subject):
+        self.subject = subject
+        self.questions = {}
+
+    def add_questions(self):
+
+    def take_quiz(self):
+
+"""A unit test will be taken here to test that the code created a successful instance of the quiz class"""
+
+
+
+
+"""Make some sample data to input for the quizes with different themes"""
+
+
+
+
+
+"""Create Main function here"""
+def main():
+
+
+
+
+
+"""Pass the main function to if__name__ statement"""
+if __name__ == "__main__":
+    main()
